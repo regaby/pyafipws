a0ca145b3c1920997736fdffb818caf9cefb3217
Gabriela Rivero <regaby@gmail.com>
2025-04-06 21:35:37 -0300

    [UPD] pyafipws 2025 RG.5616/2024

--- wsfev1.py
+++ wsfev1.py
@@ -61,6 +61,7 @@ class WSFEv1(BaseWS):
                         'ParamGetTiposPaises',
                         'ParamGetCotizacion',
                         'ParamGetPtosVenta',
+                        "ParamGetCondicionIvaReceptor",
                         'AnalizarXml', 'ObtenerTagXml', 'LoadTestXML',
                         'SetParametros', 'SetTicketAcceso', 'GetParametro',
                         'EstablecerCampoFactura', 'ObtenerCampoFactura',
@@ -134,7 +135,10 @@ class WSFEv1(BaseWS):
             cbt_desde=0, cbt_hasta=0, imp_total=0.00, imp_tot_conc=0.00, imp_neto=0.00,
             imp_iva=0.00, imp_trib=0.00, imp_op_ex=0.00, fecha_cbte="", fecha_venc_pago=None,
             fecha_serv_desde=None, fecha_serv_hasta=None, #--
-            moneda_id="PES", moneda_ctz="1.0000", caea=None, fecha_hs_gen=None, **kwargs
+            moneda_id="PES", moneda_ctz="1.0000", caea=None, fecha_hs_gen=None, 
+            cancela_misma_moneda_ext=None,
+            condicion_iva_receptor_id=None,
+            **kwargs
             ):
 
         "Creo un objeto factura (interna)"
@@ -162,11 +166,17 @@ class WSFEv1(BaseWS):
         if caea:
             fact['caea'] = caea
 
+        if cancela_misma_moneda_ext is not None:
+            fact['cancela_misma_moneda_ext'] = cancela_misma_moneda_ext
+        if condicion_iva_receptor_id is not None:
+            fact['condicion_iva_receptor_id'] = condicion_iva_receptor_id
+
         self.factura = fact
         return True
 
     def EstablecerCampoFactura(self, campo, valor):
-        if campo in self.factura or campo in ('fecha_serv_desde', 'fecha_serv_hasta', 'caea', 'fch_venc_cae', 'fecha_hs_gen'):
+        if campo in self.factura or campo in ('fecha_serv_desde', 'fecha_serv_hasta', 'caea', 'fch_venc_cae', 'fecha_hs_gen', "cancela_misma_moneda_ext",
+            "condicion_iva_receptor_id",):
             self.factura[campo] = valor
             return True
         else:
@@ -263,6 +273,8 @@ class WSFEv1(BaseWS):
                     'FchVtoPago': f.get('fecha_venc_pago'),
                     'MonId': f['moneda_id'],
                     'MonCotiz': f['moneda_ctz'],
+                    "CanMisMonExt": f.get("cancela_misma_moneda_ext"),
+                    "CondicionIVAReceptorId": f.get("condicion_iva_receptor_id"),
                     'PeriodoAsoc': {
                         'FchDesde': f['periodo_cbtes_asoc'].get('fecha_desde'),
                         'FchHasta': f['periodo_cbtes_asoc'].get('fecha_hasta'),
@@ -951,11 +963,11 @@ class WSFEv1(BaseWS):
                 for p in res['ResultGet']]
 
     @inicializar_y_capturar_excepciones
-    def ParamGetCotizacion(self, moneda_id):
+    def ParamGetCotizacion(self, moneda_id, fecha=None):
         "Recuperador de cotizaci�n de moneda"
         ret = self.client.FEParamGetCotizacion(
             Auth={'Token': self.Token, 'Sign': self.Sign, 'Cuit': self.Cuit},
-            MonId=moneda_id,
+            MonId=moneda_id, FchCotiz=fecha,
         )
         self.__analizar_errores(ret)
         res = ret['FEParamGetCotizacionResult']['ResultGet']
@@ -971,6 +983,17 @@ class WSFEv1(BaseWS):
         res = ret.get('FEParamGetPtosVentaResult', {})
         return [("%(Nro)s\tEmisionTipo:%(EmisionTipo)s\tBloqueado:%(Bloqueado)s\tFchBaja:%(FchBaja)s" % p['PtoVenta']).replace("\t", sep)
                 for p in res.get('ResultGet', [])]
+
+    @inicializar_y_capturar_excepciones
+    def ParamGetCondicionIvaReceptor(self, clase_cmp="A", sep="|"):
+        "Recuperador de valores referenciales de los identificadores de la condición frente al IVA del receptor"
+        ret = self.client.FEParamGetCondicionIvaReceptor(
+            Auth={'Token': self.Token, 'Sign': self.Sign, 'Cuit': self.Cuit},
+            ClaseCmp=clase_cmp,
+            )
+        res = ret['FEParamGetCondicionIvaReceptorResult']
+        return [(u"%(Id)s\t%(Desc)s\t%(Cmp_Clase)s" % p['CondicionIvaReceptor']).replace("\t", sep)
+                 for p in res['ResultGet']]
 
 
 def p_assert_eq(a, b):
